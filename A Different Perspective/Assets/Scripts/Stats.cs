a31313b27becdfcb835ae26b7b37c59287b94ab9
mark-Toufic <72156370+mark-Toufic@users.noreply.github.com>
2022-12-05 13:15:44 -0500

    stats

--- A Different Perspective/Assets/Scripts/Stats.cs
+++ A Different Perspective/Assets/Scripts/Stats.cs
@@ -1,18 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Stats : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float maxHealth = 100;
+    public float health = 100;
+    public float water = 100;
+    public float hunger = 100;
+    private float healthDrain = 2f;
+    private float waterDrain = 0.9f;
+    private float hungerDrain = 0.5f;
+    public Slider healthslider, hungerSlider, waterSlider;
+
+
+    private void Awake()
     {
-        
+        health = maxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        healthslider.value = health;
+        waterSlider.value = water;
+        hungerSlider.value = hunger;
+        WaterStat();
+        HungerStat();
     }
+
+
+    public void WaterStat()
+    {
+        water -= waterDrain * Time.deltaTime;
+
+        if (water < 0)
+        {
+            water = 0;
+            health -= healthDrain * Time.deltaTime;
+
+            if (health <= 0)
+            {
+                health = 0;
+                Debug.Log("Death");
+            }
+        }
+    }
+
+    public void HungerStat()
+    {
+        hunger -= hungerDrain * Time.deltaTime;
+
+        if (hunger < 0)
+        {
+            hunger = 0;
+            health -= healthDrain * Time.deltaTime;
+
+            if (health <= 0)
+            {
+                health = 0;
+                Debug.Log("Death");
+            }
+        }
+    }
+
+
+
 }
