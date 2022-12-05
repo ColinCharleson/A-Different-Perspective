9060a321fb6699312349c3e96569224758a75896
mark-Toufic <72156370+mark-Toufic@users.noreply.github.com>
2022-12-05 15:32:48 -0500

    AI

---
+++ A Different Perspective/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,149 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+
+
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    //enemy pathing
+    private EnemyState enemyState = EnemyState.patrol;
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Rigid Body
+    public Rigidbody enemy;
+
+    //states
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    //others
+    public Stats stats;
+
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+
+
+    private void FixedUpdate()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        switch (enemyState)
+        {
+
+            case EnemyState.patrol:
+                Patrolling();
+
+                if (playerInSightRange && !playerInAttackRange)
+                {
+                    enemyState = EnemyState.chase;
+                }
+
+                if (!playerInSightRange && playerInAttackRange)
+                {
+                    enemyState = EnemyState.attack;
+                }
+                break;
+
+            case EnemyState.chase:
+                ChasePlayer();
+                if (!playerInSightRange && !playerInAttackRange)
+                {
+                    enemyState = EnemyState.patrol;
+                }
+
+                if (!playerInSightRange && playerInAttackRange)
+                {
+                    enemyState = EnemyState.attack;
+                }
+
+                break;
+
+            case EnemyState.attack:
+                AttackPlayer();
+
+                if (!playerInSightRange && !playerInAttackRange)
+                {
+                    enemyState = EnemyState.patrol;
+                }
+                else if (playerInSightRange && !playerInAttackRange)
+                {
+                    enemyState = EnemyState.chase;
+                }
+
+                break;
+
+            default:
+
+                break;
+        }
+
+    }
+
+
+
+    private void Patrolling()
+    {
+        
+            if (!walkPointSet) SearchWalkPoint();
+
+            if (walkPointSet)
+            {
+                agent.SetDestination(walkPoint);
+            }
+            Vector3 distanceTowalkPoint = transform.position - walkPoint;
+
+            //if walkpoint reached
+            if (distanceTowalkPoint.magnitude < 1f)
+                walkPointSet = false;
+        
+    }
+
+    void SearchWalkPoint()
+    {
+        //caculating random point in range
+        float randZ = Random.Range(-walkPointRange, walkPointRange);
+        float randX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randX, transform.position.y, transform.position.z + randZ);
+
+        //Make sure enemy dosnt walk off map using raycast
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+        {
+            walkPointSet = true;
+        }
+    }
+
+    private void ChasePlayer()
+    {  
+            transform.LookAt(new Vector3(player.position.x, transform.position.y, player.position.z));
+            agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        transform.LookAt(player);
+    }
+    
+    public enum EnemyState
+    {
+        patrol,
+        chase,
+        attack,
+    }
+}
